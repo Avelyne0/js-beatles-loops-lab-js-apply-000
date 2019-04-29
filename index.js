7791cabfc17ecde259e4f041db03297ecede8ee5
lively-storage-3976 <sam.dbarrie@gmail.com>
2019-04-29 15:33:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function theBeatlesPlay(musicians, instruments) {
   const array = []
-  for (let i=0; n= i<musicians.length; i++) {
+  for (let i=0; i<musicians.length; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return array
